0fdff5140b86c917bb1b99ee4439a6f900a1b30a
diogo <lohn.diogo@gmail.com>
2023-04-13 14:33:13 +0000

    Added divisible function

--- project05/script.js
+++ project05/script.js
@@ -1,1 +1,12 @@
-console.log("Hello World")
+//Check if an integer is divisible by five
+function divisible(a){
+    let b = a / 5;
+    let c = (b - Math.floor(b));
+    if (c > 0){
+        return "Is not divisible by 5";
+    } else {
+        return "Is divisible by 5";
+    }
+};
+
+console.log(divisible(15));
